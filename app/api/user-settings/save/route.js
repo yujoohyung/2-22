a95a20e03ccbd19a1843636a1fede3f39a4f3fe1
yujoohyung <y132117954@google.com>
2025-09-13 16:15:47 +0900

    배포50차

--- app/api/user-settings/save/route.js
+++ app/api/user-settings/save/route.js
@@ -2,7 +2,7 @@
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
-import { requireUser } from "../../../../lib/auth-server.js";
+import { requireUser } from "../../../lib/auth-server.js";
 
 export async function POST(req) {
   try {
